3413f3712f27a6cb0a30dfe90687cd48b81bdfa0
Anderson Costa <andersscosta2@gmail.com>
2025-09-29 11:28:15 -0300

    Aula 5.1 - Exercicio com while

---
+++ aula5_Repeticoes.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número inteiro: "))
+contador  = 1 #de onde a contagem começa
+while contador <= 10:
+    print(numero, '+', contador, '=',numero + contador)
+    contador = contador + 1
+
